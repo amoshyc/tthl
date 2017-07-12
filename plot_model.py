962d2d8cc2ab0db41f42c0b58e3d804e964ff007
amoshyc <amoshuangyc@gmail.com>
2017-07-12 22:54:49 +0800

    Update at 2017-07-12 22-54-49

--- plot_model.py
+++ plot_model.py
@@ -4,4 +4,4 @@ from keras.models import load_model
 from keras.utils import plot_model
 
 model = load_model(argv[1], compile=False)
-plot_model(model, to_file='{}.png'.format(argv[2]), show_shapes=True)
+plot_model(model, to_file=argv[2], show_shapes=True, show_layer_names=False)
