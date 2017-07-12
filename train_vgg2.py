a78eb3e4681ddf8733a2eea0291ce6e40243d8e2
amoshyc <amoshuangyc@gmail.com>
2017-07-12 19:29:32 +0800

    Update at 2017-07-12 19-29-32

--- train_vgg2.py
+++ train_vgg2.py
@@ -47,14 +47,14 @@ class MyLogger(Callback):
 
 def get_model():
     model = Sequential()
-    model.add(Conv2D(8, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)))
-    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(Conv2D(4, 5, 5, activation='relu', input_shape=(28, 28, 1)))
+    model.add(Conv2D(8, 3, 3, activation='relu'))
+    model.add(Conv2D(16, 3, 1, activation='relu'))
     model.add(Flatten())
-    model.add(Dense(50))
+    model.add(Dense(30))
     model.add(Dropout(0.3))
-    model.add(Dense(2, activation='softmax'))
-
+    model.add(Dense(1, activation='sigmoid'))
+    return model
 
 def main():
     with tf.device('/gpu:3'):
