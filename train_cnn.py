d827ab5c78c9ece0f9fbe2f938b68963fd24a899
amoshyc <amoshuangyc@gmail.com>
2017-07-12 19:53:56 +0800

    Update at 2017-07-12 19-53-56

--- train_cnn.py
+++ train_cnn.py
@@ -16,33 +16,8 @@ from keras.preprocessing import image
 from keras.layers import *
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import *
-from keras.callbacks import Callback, ModelCheckpoint
+from keras.callbacks import Callback, ModelCheckpoint, CSVLogger
 from keras.applications.vgg16 import VGG16
-
-
-class MyLogger(Callback):
-    def __init__(self, prefix=None):
-        self.train_loss = []
-        self.train_acc = []
-        self.loss_path = Path('{}_loss.json'.format(prefix))
-        self.acc_path = Path('{}_acc.json'.format(prefix))
-
-        # self.loss_path = self.loss_path.resolve()
-        # self.acc_path = self.acc_path.resolve()
-        # self.loss_path.parent.mkdir(parents=True, exist_ok=True)
-        # self.acc_path.parent.mkdir(parents=True, exist_ok=True)
-
-    def on_epoch_end(self, epoch, logs):
-        self.train_loss.append(logs.get('loss'))
-        self.train_acc.append(logs.get('acc'))
-
-        df_loss = pd.DataFrame()
-        df_loss['train_loss'] = self.train_loss
-        df_loss.to_json(str(self.loss_path), orient='split')
-
-        df_loss = pd.DataFrame()
-        df_loss['train_acc'] = self.train_acc
-        df_loss.to_json(str(self.acc_path), orient='split')
 
 
 def get_model():
@@ -61,7 +36,6 @@ def main():
     with tf.device('/gpu:3'):
         model = get_model()
 
-    opt = RMSprop()
     model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['binary_accuracy'])
     model.summary()
 
@@ -78,7 +52,7 @@ def main():
         'validation_data': (x_val, y_val),
         'shuffle': True,
         'callbacks': [
-            MyLogger(prefix='cnn'),
+            CSVLogger('cnn.log'),
             ModelCheckpoint(filepath="./cnn_epoch{epoch:02d}_{loss:.3f}.h5")
         ]
     } # yapf: disable
