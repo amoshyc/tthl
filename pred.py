4513f9392024f14a44964980b6d6cdd7a021f2cb
amoshyc <amoshuangyc@gmail.com>
2017-07-13 16:40:15 +0800

    Update at 2017-07-13 16-40-15

--- pred.py
+++ pred.py
@@ -8,7 +8,7 @@ model = load_model(argv[1])
 
 pos = Path('~/dataset/video01')
 x_val = sorted((pos / 'frames/').iterdir())
-y_val = json.load((pos / 'info').open())
+y_val = json.load((pos / 'info').open())['label']
 
 res = model.evaluate(x_val, y_val)
 print(res)
